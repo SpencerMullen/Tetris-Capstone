0c73b699a9d6a1bf007bbabf70f682cefe5feceb
Mars7543 <miguelaroberts09@gmail.com>
2020-02-19 21:40:47 -0500

    create tetris homepage

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
-const websockets    = require('./websockets/main')
 const express       = require('express')
 const path          = require('path')
+
+// start websocket
+require('./websockets/main')
 
 const port  = process.env.PORT || 8000
 const app   = express()
 
+app.get('/', (req, res) => res.redirect('/index.html'))
 app.use(express.static(path.join(__dirname, '..', 'client')))
 
 app.listen(port, () => console.log(`App running on localhost:${port}....`))
