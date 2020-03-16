d0b9f6b7ee3b4bd3fbc641357428a8b18025cb72
Mars7543 <miguelaroberts09@gmail.com>
2020-03-15 21:33:49 -0400

    Added password encryption

--- server/api/login.js
+++ server/api/login.js
@@ -1,21 +1,25 @@
-const User = require('../models/User')
-const router = require('express').Router()
+const User      = require('../models/User')
+const router    = require('express').Router()
+const bcrypt    = require('bcryptjs')
 
 // POST /api/login
 // Login user
 router.post('/', async (req, res) => {
-    const userInfo = { email, password } = req.body
+    const { email, password } = req.body
+
+    if (!email || !password) return res.status(400).send({ msg: 'Please Enter All Fields' })
 
     try {
-        const user = await User.findOne(userInfo)
-        
-        if (!user) return res.send({ msg: 'Error Logging In' })
+        const user = await User.findOne({ email })
+
+        const isMatched = await bcrypt.compare(password, user.password || '')
+        if (!isMatched) return res.status(400).json({ msg: 'Email or password is incorrect.' })
+
         res.send(user)
     } catch (e) {
+        console.log(e)
         res.status(500).send({ msg: e })
     }
 })
 
 module.exports = router
-
-// TODO: check that email, and password fields are not empty
--- server/api/register.js
+++ server/api/register.js
@@ -1,13 +1,30 @@
-const User = require('../models/User')
-const router = require('express').Router()
+const User      = require('../models/User')
+const router    = require('express').Router()
+const bcrypt    = require('bcryptjs')
 
 // GET /api/register
 // Register user
 router.post('/', async (req, res) => {
     const userInfo = { username, email, password } = req.body
 
+    if (!username || !email || !password) return res.status(400).send({ msg: 'Please Enter All Fields' })
+
     try {
+
+        const user_email = await User.findOne({ email })
+        const user_username = await User.findOne({ username })
+
+        if (user_email) return res.status(400).send({ msg: 'Email already taken' })
+        if (user_username) return res.status(400).send({ msg: 'Username already taken' })
+
         const newUser = new User(userInfo)
+
+        // Create password hash
+        const salt = await bcrypt.genSalt()
+        const hash = await bcrypt.hash(password, salt)
+
+        newUser.password = hash
+
         await newUser.save()
 
         res.send(newUser)
@@ -17,7 +34,3 @@ router.post('/', async (req, res) => {
 })
 
 module.exports = router
-
-// TODO: check that username, email, and password fields are not empty
-// TODO: check to make sure user with given email / username doesn't already exist
-// TODO: add password encryption
